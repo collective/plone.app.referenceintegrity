8bd0e212e5526b7be0d824197c0e49a008ecc6ec
Godefroid Chapelle <gotcha@bubblenet.be>
2011-08-31 14:21:14 +0000

    support ReferenceableVocabularies
    svn path=/plone.app.referenceintegrity/trunk/; revision=51865

--- plone/app/referenceintegrity/interfaces.py
+++ plone/app/referenceintegrity/interfaces.py
@@ -2,6 +2,8 @@ from zope.interface import Interface
 from zope.schema import List
 from zope.schema import TextLine
 
+from Products.Archetypes.interfaces import IVocabulary
+
 
 class ISettings(Interface):
 
@@ -10,3 +12,10 @@ class ISettings(Interface):
         description=u"Reference types that should be protected",
         required=False,
         value_type=TextLine())
+
+
+class IReferenceableVocabulary(IVocabulary):
+    """vocabulary used in referenceintegrity"""
+
+    def getObjectsSet(content_instance, values):
+        """returns the set of objects corresponding to the values"""
